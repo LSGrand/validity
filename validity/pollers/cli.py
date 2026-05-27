3c24a8202b6de3e2e04c73ee4dfdb6e4398d4194
LS Grand <lsg@lsgrand.com>
2026-05-27 13:20:01 +0300

    Fix s500 not backuping

--- validity/pollers/cli.py
+++ validity/pollers/cli.py
@@ -1,3 +1,5 @@
+import re
+
 from typing import TYPE_CHECKING
 
 from netmiko import BaseConnection, ConnectHandler
@@ -15,4 +17,5 @@ class NetmikoPoller(ConsecutivePoller):
     driver_factory = ConnectHandler
 
     def poll_one_command(self, driver: BaseConnection, command: "Command") -> str:
-        return driver.send_command(command.parameters["cli_command"])
+        custom_expect = rf"{re.escape(driver.base_prompt)}.*#"
+        return driver.send_command(command.parameters["cli_command"],expect_string=custom_expect,read_timeout=60,delay_factor=2)
